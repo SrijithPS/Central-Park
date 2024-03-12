1e57611494ef22ff80f913e604dd542326418209
SrijithPS <srijithsps@gmail.com>
2024-03-12 21:49:54 +0530

    cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { ProductProps } from "./Products";
 import { Products } from "./Products";
+import { Card } from "react-bootstrap";
 export function Cart() {
   const [cart, setCart] = useState<ProductProps[]>([]);
    function addToCart(product: ProductProps) {
@@ -12,8 +13,18 @@ export function Cart() {
     <div>
     <h2>My Cart</h2>
     <ul>
-      {cart.map((item,index)=>(
-        <li key={index}>{item.name}</li>
+      {cart.map((item)=>(
+       <div className="card-layout">
+       <Card className="cards" key={item.id}>
+         <Card.Img src={item.image} />
+         <Card.Body>
+           <Card.Title>{item.name}</Card.Title>
+           <Card.Text> &#8377; {item.pricePaisa / 100}</Card.Text>
+           <p></p>
+          
+         </Card.Body>
+       </Card>
+     </div>
       )
 
       )}
